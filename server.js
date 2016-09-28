ac672752b9bda40c340d73da88784e57e7207172
harshan789 <harshan789@gmail.com>
2016-09-28 14:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var article= {
+var articles= {
+ 'article': {
     title: 'article one- Harshan',
     heading:'article',
     date:'sep 5 2016',
@@ -18,9 +19,28 @@ var article= {
               <p>
               This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
               </p>`
+     
+ },
           
          
-};
+  'two': { 
+     title: 'article two- Harshan',
+    heading:'two',
+    date:'sep 10 2016',
+    content:`<p>
+              This is the content of my second article.This is the content of my second article.This is the content of my second article.</p>`
+              },
+ 'three':{
+     title: 'article three- Harshan',
+    heading:'two',
+    date:'sep 15 2016',
+    content:`<p>
+              This is the content of my third article.This is the content of my third article.This is the content of my third article.
+              </p>`
+              }
+              
+ };
+ 
 function createTemplate (data){
     var title=data.title;
     var date =data.date;
@@ -63,8 +83,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article',function(req,res){
-    res.send(createTemplate(article));
+app.get('/:articleName',function(req,res){
+    //articleName will be article
+    //articles[articleName]={} content object for article
+    var articleName=req.param.articleName;
+    res.send(createTemplate(articles[articlename]));
 });
 app.get('/two',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'two.html'));
